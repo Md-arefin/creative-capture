d1af65d1796d0aa49d1ccf57a7a5f2f2200b46fa
Md-arefin <ansariarefin617@gmail.com>
2023-06-07 12:03:45 +0600

    error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import Main from "../Layout/Main";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import { Link } from 'react-router-dom';
+
+
+const ErrorPage = () => {
+    return (
+        <div className='relative'>
+            <div>
+                <Player
+                    autoplay
+                    loop
+                    src="https://assets4.lottiefiles.com/private_files/lf30_bfxeb0to.json"
+                    className='xl:w-[1000px] lg:w-[800px] md:w-[700px] sm:w-[450px]
+                 w-[350px]'
+                >
+                    <Controls visible={!true} buttons={['play', 'repeat', 'frame', 'debug']} />
+                </Player>
+            </div>
+            <Link to='/'>
+                <div className='text-center'>
+                    <button className="btn bg-yellow-500 border-0 text-black ">Back to Home</button>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
