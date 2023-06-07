781a14d1f3f54de554456c2ab05f0d0b52c18fa6
Md-arefin <ansariarefin617@gmail.com>
2023-06-07 14:54:02 +0600

    Footer added

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4'>
     <React.StrictMode>
       <RouterProvider router={router} />
-    </React.StrictMode>,
+    </React.StrictMode>
   </div>
 )
