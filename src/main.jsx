844dc4d85ed5ecaf767732bf87ea95b0736a6d5a
Md-arefin <ansariarefin617@gmail.com>
2023-06-07 12:18:14 +0600

    navbar implemented

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import { router } from './Routes/Routes';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-   <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className='max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )
